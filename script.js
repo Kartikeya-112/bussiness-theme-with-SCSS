5b28bbaa5933e7c7f277580605eeb1368d01b875
Kartikeya Tiwari <ktiwari@lutron.com>
2026-02-10 17:53:45 +0530

    Updated Changes as told by Mayank

--- script.js
+++ script.js
@@ -38,3 +38,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   window.addEventListener("scroll", onScroll);
 });
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("navMenu");
+
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
